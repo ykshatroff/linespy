81d1519a75f8b8e5d981ebd644dd800d7c9a993b
Yuriy Shatrov <yuriy.shatrov@wolt.com>
2023-04-02 15:44:03 +0300

    Version 0.02: Added grid lines.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,17 @@ import pygame
 
 SCREEN_SIZE = 360, 360  # X, Y in pixels
 SCREEN_BACKGROUND_COLOR = 100, 200, 255  # Red, Green, Blue (0-255)
+GRID_LINE_COLOR = 255, 255, 255  # white
+
+
+def draw_grid(screen):
+    # Draw vertical grid lines
+    for x in range(40, 360, 40):
+        pygame.draw.line(screen, GRID_LINE_COLOR, (x, 0), (x, 360))
+
+    # Draw horizontal grid lines
+    for y in range(40, 360, 40):
+        pygame.draw.line(screen, GRID_LINE_COLOR, (0, y), (360, y))
 
 
 def main():
@@ -11,6 +22,7 @@ def main():
     screen = pygame.display.set_mode(SCREEN_SIZE)
     time.sleep(0.1)
     screen.fill(SCREEN_BACKGROUND_COLOR)
+    draw_grid(screen)
     pygame.display.flip()
 
     running = True
